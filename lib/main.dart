a3c430c4d884eaa43a896acdd5d7c1dfd2f99dde
matbuendia <buendiamateo670@gmail.com>
2026-03-06 19:44:06 -0500

    Actualizacion del repositorio

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       title: 'Mi proyecto',
 
-      home: const LoginScreen(),
+      home: const PlannerScreen(),
     );
   }
 }
