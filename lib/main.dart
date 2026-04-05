2b79d6c1fc22851799f5dcfb5ef2184f82846ca6
matbuendia <buendiamateo670@gmail.com>
2026-04-05 12:14:41 -0400

    creacion de carpeta screens_students y movimiento de las pantallas curse y planner

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
-import 'screens/curse_screen.dart';
-import 'screens/planner_screen.dart';
+//import 'screens/login_screen.dart';
+import 'screens/students_screens/planner_screen.dart';
+//import 'screens/students_screens/curse_screen.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp( 
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Mi proyecto',
 
